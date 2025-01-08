9935139c514073a45f94775ac386950670be4bd7
cmaykr <dj@netatonce.net>
2025-01-08 16:25:46 +0100

    Removes unused socket classes and adds basic communication between client and server

--- client.cpp
+++ client.cpp
@@ -1,7 +1,63 @@
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <charconv>
+#include <unistd.h>
+#include <stdexcept>
 #include <iostream>
+#include <poll.h>
+
+#include <chrono>
+#include <thread>
+
 
 #include "conn.hpp"
+
+int clientFD;
+
+void connectToServer(std::string const& address, int port)
+{
+    struct addrinfo hints{};
+    struct addrinfo *result, *rp;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_ADDRCONFIG;
+
+    char buf[4];
+    std::to_chars(buf, buf + sizeof(buf), port);
+    int err;
+    if (err = getaddrinfo(address.c_str(), buf, &hints, &result) < 0)
+    {
+        std::cout << "Error: " << errno << std::endl;
+        return;
+    }
+
+    for (rp = result; rp; rp = rp->ai_next)
+    {
+        clientFD = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+        if (clientFD == -1)
+        {
+            continue;
+        }
+        if (connect(clientFD, rp->ai_addr, rp->ai_addrlen) == 0)
+        {
+            char host[NI_MAXHOST], service[NI_MAXSERV];
+            int s = getnameinfo(rp->ai_addr, rp->ai_addrlen, host, NI_MAXHOST, service, NI_MAXSERV, NI_NUMERICSERV);
+            break;
+        }
+        
+        //close(clientFD);
+    }
+
+    freeaddrinfo(result);
+
+    std::cout << "Socket FD: " << clientFD << std::endl;
+}
 
 int main(int argc, char *argv[])
 {
@@ -20,13 +76,34 @@ int main(int argc, char *argv[])
     address = argv[1]; // TODO: Check for correct format.
     port = std::stoi(argv[2]); // TODO: Check for correct format and port number.
 
+    connectToServer(address, port);
 
-    Connection conn{};
+    bool gameOver = false;
+    while (gameOver == false)
+    {
+        char buf[1024];
+        struct pollfd fds[1];
+        fds[0].fd = clientFD;
+        fds[0].events = POLLIN;
+        int N{};
+        if (poll(fds, 1, -1) > 0)
+            N = recv(clientFD, buf, 1024, 0);
+        else    
+        {
+            std::cout << "Something went wrong" << std::endl;
+            return -1;
+        }
 
-    conn.connectToServer(address, port);
+        std::string rec {buf, N};
+        if (N == -1)
+        {
+            std::cout << "Connection failed" << std::endl;
+        }
 
-    conn.sendMessage("Test");
-
-    conn.closeConnection();
+        std::cout << "Recieved: " << N << " bytes" << std::endl;
+        std::cout << rec;
+        std::cout << std::endl;
+    }
+    close(clientFD);
     return 0;
 }
--- game.hpp
+++ game.hpp
@@ -12,10 +12,19 @@ public:
     void startGame();
 
 
+    /// @brief Places either an x or 0 on the chosen spot, depending on the current player that is placing.
+    /// @param spot The spot 1-9 to place the mark on.
+    /// @return True if the play succeeded or false if the spot was taken or out of bounds.
     bool playMove(int spot);
+    /// @brief Checks which player's turn it is.
+    /// @return True if it's the first players turn, false if it's the second players turn.
     bool isPlayerOnesTurn() const;
+    /// @brief Checks if the game is over, i.e one player has three in a row.
+    /// @return True if the game is over, false otherwise.
     bool isOver() const;
 
+    /// @brief Gets the board in a string format.
+    /// @return The board.
     std::string getBoard() const;
 private:
     void drawGameBoardToOstream(std::ostream & out) const;
